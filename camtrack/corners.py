2da96282d23d996924383b0b4db552dc3133f151
SBOne-Kenobi <sbonekenobi@gmail.com>
2021-10-03 16:22:32 +0300

    homework 1 done

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -46,19 +46,111 @@ class _CornerStorageBuilder:
         return StorageImpl(item[1] for item in sorted(self._corners.items()))
 
 
+_QUALITY = 0.05
+_MIN_DIST = 6
+_CORNER_SIZE = 10
+_PYR_SIZE = 3
+_WINDOW_SIZE = _CORNER_SIZE * (2 ** _PYR_SIZE)
+
+
+def _detect_corners_pyr(image, ids, corners, sizes):
+    mask = np.full_like(image, 255, dtype='uint8')
+    if corners is not None:
+        for corner, size in zip(corners, sizes):
+            cv2.circle(mask, corner, int(_MIN_DIST * size[0] / _CORNER_SIZE), 0, -1)
+
+    orig_size = image.shape[0]
+
+    ext_corners = []
+    cur_img = image.copy()
+    cur_mask = mask.copy()
+
+    for _ in range(_PYR_SIZE):
+        cur_corners, cur_quality = cv2.goodFeaturesToTrackWithQuality(
+            image=cur_img,
+            mask=cur_mask,
+            maxCorners=0,
+            qualityLevel=_QUALITY,
+            minDistance=_MIN_DIST,
+            blockSize=_CORNER_SIZE
+        )
+        if cur_corners is not None:
+            cur_corners = (cur_corners * orig_size / cur_img.shape[0]).astype(int)
+            cur_size = int(_CORNER_SIZE * orig_size / cur_img.shape[0])
+
+            ext_corners.extend(zip(cur_corners, [cur_size] * len(cur_corners), cur_quality))
+
+        cur_img = cv2.pyrDown(cur_img)
+        cur_mask = cv2.pyrDown(cur_mask)
+
+    sorted_ext = sorted(ext_corners, key=lambda x: (-x[2][0], x[1]))
+
+    next_id = 0 if ids is None else ids.max() + 1
+    ext_ids = []
+    ext_corners = []
+    ext_sizes = []
+
+    for corner, size, _ in sorted_ext:
+        corner = corner[0]
+        if mask[corner[1], corner[0]] == 0:
+            continue
+        cv2.circle(mask, corner, int(_MIN_DIST * size / _CORNER_SIZE), 0, -1)
+        ext_ids.append(next_id)
+        ext_corners.append(corner)
+        ext_sizes.append(size)
+        next_id += 1
+
+    if not ext_ids:
+        return ids, corners, sizes
+
+    ext_ids = np.array(ext_ids).reshape([-1, 1])
+    ext_corners = np.array(ext_corners)
+    ext_sizes = np.array(ext_sizes).reshape([-1, 1])
+
+    if ids is None:
+        ids = ext_ids
+        corners = ext_corners
+        sizes = ext_sizes
+    else:
+        ids = np.vstack([ids, ext_ids])
+        corners = np.vstack([corners, ext_corners])
+        sizes = np.vstack([sizes, ext_sizes])
+
+    return ids, corners, sizes
+
+
+def _detect_flow(image0, image1, ids, corners, sizes):
+    def to_uint8(img):
+        return (img * 255).astype('uint8')
+
+    next_corners, st, err = cv2.calcOpticalFlowPyrLK(
+        to_uint8(image0), to_uint8(image1), corners.astype('float32'), None,
+        winSize=[_WINDOW_SIZE] * 2, maxLevel=_PYR_SIZE
+    )
+
+    st = st.flatten() == 1
+
+    ids = ids[st]
+    next_corners = next_corners[st].astype(int)
+    sizes = sizes[st]
+
+    if len(ids) == 0:
+        return None, None, None
+
+    return ids, next_corners, sizes
+
+
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
-    # TODO
-    image_0 = frame_sequence[0]
-    corners = FrameCorners(
-        np.array([0]),
-        np.array([[0, 0]]),
-        np.array([55])
-    )
-    builder.set_corners_at_frame(0, corners)
-    for frame, image_1 in enumerate(frame_sequence[1:], 1):
-        builder.set_corners_at_frame(frame, corners)
-        image_0 = image_1
+    ids = None
+    corners = None
+    sizes = None
+    for frame, image in enumerate(frame_sequence):
+        if frame > 0:
+            ids, corners, sizes = _detect_flow(frame_sequence[frame - 1], image, ids, corners, sizes)
+        ids, corners, sizes = _detect_corners_pyr(image, ids, corners, sizes)
+
+        builder.set_corners_at_frame(frame, FrameCorners(ids, corners, sizes))
 
 
 def build(frame_sequence: pims.FramesSequence,
